0500834492406fa9b27f8816110b895b3c0591f4
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-09-30 13:27:05 +0200

    ok

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-#STI Project
+# STI Project
 
-##TO DO
+## ToDo
 
-  1. clean code
-  2. prepare statement 
-  3. comments
+1. clean code
+2. prepare statement 
+3. Comments
 
 ## Use case
 
+First, use dock.sh 
+
+Now there are 4 accounts. admin, lio, jere and jee, jee has low rights. The password for each account is the pseudo.
+
+You can try the app    http://localhost:8080
--- site/databases/test.php
+++ site/databases/test.php
@@ -4,7 +4,7 @@ $res=$db->exec("CREATE TABLE USER (id INTEGER PRIMARY KEY, pseudo TEXT NOT NULL,
 $res=$db->exec("CREATE TABLE CHAT (id INTEGER PRIMARY KEY, idsend INTEGER NOT NULL, idreceive INTEGER NOT NULL, subject TEXT NOT NULL, messages TEXT NOT NULL,  Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);");
 $res=$db->exec("INSERT INTO USER"."(pseudo, passwd, validity, roles)"."VALUES ('admin', 'admin', '1' , 'admin');");
 $res=$db->exec("INSERT INTO USER"."(pseudo, passwd, validity, roles)"."VALUES ('lio', 'lio', '1' , 'admin');");
-$res=$db->exec("INSERT INTO USER"."(pseudo, passwd, validity, roles)"."VALUES ('marie', 'marie', '1' , 'low');");
+$res=$db->exec("INSERT INTO USER"."(pseudo, passwd, validity, roles)"."VALUES ('jere', 'jere', '1' , 'admin');");
 $res=$db->exec("INSERT INTO USER"."(pseudo, passwd, validity, roles)"."VALUES ('jee', 'jee', '1' , 'low');");
 $db->close();
 
