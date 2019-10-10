fda88f4024dab1c3571dbe955096c747942275b8
jerozerbib <jerozerbib@gmail.com>
2019-10-10 16:21:29 +0200

    Update script and make runnable dock.sh

--- linux.sh
+++ linux.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-rm site/databases/test.db
+if test -f site/databases/test.db; then
+    rm site/databases/test.db
+
+else
+    echo "File does not exist so let's move on shall we"
+fi
+
 php site/databases/test.php
+mv test.db site/databases/test.db
 
 chmod 777 site/databases
 chmod 777 site/databases/test.db
--- site/databases/test.php
+++ site/databases/test.php
@@ -1,5 +1,5 @@
 <?php
-$db = new SQLite3('test.db');
+$db = new SQLite3('./test.db');
 $res=$db->exec("CREATE TABLE USER (id INTEGER PRIMARY KEY, pseudo TEXT NOT NULL, passwd TEXT NOT NULL, validity INTEGER NOT NULL, roles TEXT NOT NULL);");
 $res=$db->exec("CREATE TABLE CHAT (id INTEGER PRIMARY KEY, idsend INTEGER NOT NULL, idreceive INTEGER NOT NULL, subject TEXT NOT NULL, messages TEXT NOT NULL,  Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);");
 $res=$db->exec("INSERT INTO USER"."(pseudo, passwd, validity, roles)"."VALUES ('admin', 'admin', '1' , 'admin');");
